30fbb0514dd4609ef2f3bac6853b79ae50dfb30c
Youness Dominique <youness.dominique@gmail.com>
2025-05-28 13:43:53 +0200

    feat: create home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\event;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function event()
+    public function event($reference)
     {
-        return view('pages.evenement');
+        $event = Event::where('reference', $reference)->firstOrFail();
+
+        return view('pages.evenement', compact('event'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/evenement/{reference}', [HomeController::class, 'event'])->name('event.detail');
