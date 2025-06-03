57ab1e6a63484682a854b5f011ef4525b713a8e1
Youness Dominique <youness.dominique@gmail.com>
2025-06-03 11:18:02 +0200

    style: add loading to add button

--- resources/views/filament/resources/event-resource/pages/manage-guests.blade.php
+++ resources/views/filament/resources/event-resource/pages/manage-guests.blade.php
@@ -3,8 +3,14 @@
         <form wire:submit.prevent="create" class="space-y-4">
             {{ $this->form }}
 
-            <x-filament::button type="submit">
-                Ajouter l'invité
+            <x-filament::button
+                type="submit"
+                wire:target="create"
+                wire:loading.attr="disabled"
+                wire:loading.class="opacity-50 cursor-not-allowed"
+            >
+                <span wire:loading.remove wire:target="create">Ajouter l'invité</span>
+                <span wire:loading wire:target="create">Chargement...</span>
             </x-filament::button>
         </form>
 
